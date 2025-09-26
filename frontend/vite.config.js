28f04d567fe0fe7e44981d5f14915e90aba9ae26
Alex DJOUSSE TEDONGMENE <alex.djousse@student.unamur.be>
2025-09-26 21:56:28 +0200

    Refactor code structure for improved readability and maintainability

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -39,7 +39,6 @@ export default defineConfig({
       },
       output: {
         manualChunks: {
-          vendor: ['swiper', 'aos'],
           utils: ['./js/utils.js'],
           api: ['./js/apiService.js'],
           ui: ['./js/ui.js'],
